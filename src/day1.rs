3de1765e74cff20cce26c464bbe1c91e73fde33b
Sychic <47618543+Sychic@users.noreply.github.com>
2022-07-10 10:04:04 -0400

    Day 1, Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,10 @@
+#[allow(dead_code)]
 pub mod part1 {
     use std::fs;
 
     use itertools::Itertools;
 
     pub fn main() {
-        println!("Please enter data: (end by typing `end`)");
         let text = read_file();
         let data = text.split("\n").map(parse_int).collect_vec();
         let changes = to_changes(data);
@@ -12,12 +12,12 @@ pub mod part1 {
         println!("There are {:?} increments and {:?} decrements", increments, decrements)
     }
     
-    fn read_file() -> String {
+    pub(super) fn read_file() -> String {
         fs::read_to_string("day1.txt")
         .expect("uh oh, something broke!")
     }
     
-    fn parse_int(string: &str) -> i32 {
+    pub(super) fn parse_int(string: &str) -> i32 {
         let res = string.trim().parse::<i32>();
         if res.is_err() {
             println!("parse error: {string}");
@@ -25,12 +25,12 @@ pub mod part1 {
         res.unwrap()
     }
     
-    enum Change {
+    pub(super) enum Change {
         Increase,
         Decrease
     }
     
-    fn to_changes(list: Vec<i32>) -> Vec<Change> {
+    pub(super) fn to_changes(list: Vec<i32>) -> Vec<Change> {
         let window_iter = list.as_slice().windows(2);
         let mut res: Vec<Change> = Vec::new();
         for window in window_iter {
@@ -40,7 +40,7 @@ pub mod part1 {
         res
     }
     
-    fn count_changes(changes: Vec<Change>) -> (i32, i32) {
+    pub(super) fn count_changes(changes: Vec<Change>) -> (i32, i32) {
         changes.iter()
             .fold(
                 (0, 0), 
@@ -54,5 +54,42 @@ pub mod part1 {
                     step
                 }
             )
+    }
+}
+
+#[allow(dead_code)]
+pub mod part2 {
+    use itertools::Itertools;
+
+    use crate::day1::part1;
+
+    pub fn main() {
+        let text = part1::read_file();
+        let data = text.split("\n");
+        let summed_data = sum_windows(data.collect_vec(), 3);
+        let changes = part1::to_changes(summed_data);
+        let (increments, decrements) = part1::count_changes(changes);
+        println!("There are {:?} increments and {:?} decrements", increments, decrements)
+    }
+
+    fn sum_arr(array: &[i32]) -> i32 {
+        let mut res = 0;
+        for ele in array {
+            res += ele;
+        }
+        res
+    }
+
+    fn sum_windows(data: Vec<&str>, window_size: usize) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+        let windows = data.windows(window_size);
+        for ele in windows {
+            let mut sum = 0;
+            for string in ele {
+                sum += part1::parse_int(*string);
+            }
+            res.push(sum);
+        }
+        res
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod day1;
 fn main() {
     println!("Hello, world!");
-    day1::part1::main();
+    day1::part2::main();
 }
